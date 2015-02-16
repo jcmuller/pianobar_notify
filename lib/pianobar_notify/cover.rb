0f8ec5c261bb0650a1875c87e9d847e7c11e05c9
Juan C. Müller <jcmuller@gmail.com>
2015-02-15 21:45:35 -0500

    Clean up

--- lib/pianobar_notify/cover.rb
+++ lib/pianobar_notify/cover.rb
@@ -3,9 +3,7 @@ require 'mini_magick'
 require 'tempfile'
 
 module PianobarNotify
-    #coverArt=http://cont-sjl-1.pandora.com/images/public/amz/5/2/0/4/037627044025_500W_500H.jpg
   class Cover
-    #puts blob.data.cover_art
     def path
       confirm_it_exists
       image_name
@@ -16,9 +14,7 @@ module PianobarNotify
     attr_reader :temp_image
 
       def confirm_it_exists
-        unless File.exists?(image_path)
-          download_and_convert
-        end
+        download_and_convert unless File.exists?(image_path)
       end
 
       def download_and_convert
